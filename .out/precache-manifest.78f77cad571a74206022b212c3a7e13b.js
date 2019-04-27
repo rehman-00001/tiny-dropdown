8e9886b2f87720aa213d71c9006d93a20e6f7027
Abdul Rehman <m.n_abdulrehman@ymail.com>
2019-04-27 13:50:05 +0530

    new-update: panel-only mode

---
+++ .out/precache-manifest.78f77cad571a74206022b212c3a7e13b.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = [
+  {
+    "url": "vendors~main.433a590290959ac09d1d.bundle.js"
+  },
+  {
+    "url": "runtime~main.433a590290959ac09d1d.bundle.js"
+  },
+  {
+    "revision": "6fed24c6f61812e9697b",
+    "url": "main.css"
+  },
+  {
+    "url": "main.433a590290959ac09d1d.bundle.js"
+  },
+  {
+    "revision": "96036659f2220e67c775b38cd8afda41",
+    "url": "iframe.html"
+  }
+];
